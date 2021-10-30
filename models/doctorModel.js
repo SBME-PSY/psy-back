81c8b51a268b9f12ca6d3b3410cac96a7fc00434
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-10-30 19:54:26 +0200

    feat: add doctor initials as profile picture

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 const preSave = require('../utils/preSave');
 
 const doctorSchema = new mongoose.Schema({
@@ -14,7 +15,7 @@ const doctorSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['doctor', 'user', 'admin'],
-    default: 'user',
+    default: 'doctor',
   },
   password: {
     type: String,
@@ -61,10 +62,24 @@ const doctorSchema = new mongoose.Schema({
     type: String,
     required: [true, 'please enter your cv'],
   },
+  picture: {
+    type: String,
+  },
 });
 //hash the password
 doctorSchema.pre('save', preSave.cryptPassword);
 doctorSchema.pre('save', preSave.setTimePasswordChangedAt);
+doctorSchema.pre('save', function () {
+  if (!this.picture) {
+    const uri = 'https://avatars.dicebear.com/api/initials/';
+    const initials = this.name
+      .split(' ')
+      .reduce((init, str) => init + str[0], '');
+    const seed = crypto.randomBytes(5).toString('hex');
+    const picURL = `${uri + initials + seed}.svg`;
+    this.picture = picURL;
+  }
+});
 
 const doctorModel = mongoose.model('doctorModel', doctorSchema);
 
