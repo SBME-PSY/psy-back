745b2fd0203e03f1883bd05d5f9ba29378fd17bf
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-11-14 11:48:45 +0200

    feat: finish doctor signup form validation

--- validators/doctorValidators/doctorSignupValidations.js
+++ validators/doctorValidators/doctorSignupValidations.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const AppError = require('../../utils/appError');
 
 const maritalStatus = [
   'Single',
@@ -13,10 +14,26 @@ const sex = ['Male', 'Female'];
 
 exports.doctorSignupValidationScheme = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().email().normalize().lowercase().required(),
-  password: Joi.string().required(),
+  email: Joi.string()
+    .email()
+    .error(() => new AppError('Please enter a valid email address', 400))
+    .normalize()
+    .lowercase()
+    .required(),
+  password: Joi.string()
+    .required()
+    .pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+    .error(
+      () =>
+        new AppError(
+          'Password must be at least 8 characters long and contain at least one number, one uppercase letter and one special character',
+          400
+        )
+    ),
   confirmPassword: Joi.ref('password'),
-  phone: Joi.string(),
+  phone: Joi.string()
+    .pattern(new RegExp('^(\\+2)?01[0-25]\\d{8}$'))
+    .error(() => new AppError('Invalid Egyptian Phone Number', 400)),
   address: Joi.string(),
   sex: Joi.string().valid(...sex),
   maritalStatus: Joi.string().valid(...maritalStatus),
@@ -25,7 +42,12 @@ exports.doctorSignupValidationScheme = Joi.object({
 });
 
 exports.doctorLoginValidationScheme = Joi.object({
-  email: Joi.string().email().normalize().lowercase().required(),
+  email: Joi.string()
+    .email()
+    .error(() => new AppError('Please enter a valid email address', 400))
+    .normalize()
+    .lowercase()
+    .required(),
   password: Joi.string().required(),
   role: Joi.string().equal('doctor'),
 });
