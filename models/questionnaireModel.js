764435f933aba5b6fd65ed1e3ca2bbd3f5a2ac67
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-12-28 10:46:19 +0200

    feat: add questionnaire model

---
+++ models/questionnaireModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema(
+  {
+    body: String,
+    weight: Number,
+  },
+  { timestamps: true }
+);
+
+const questionSchema = new mongoose.Schema(
+  {
+    body: String,
+    answers: [answerSchema],
+  },
+  { timestamps: true }
+);
+
+const scoreSchema = new mongoose.Schema(
+  {
+    min: Number,
+    max: Number,
+    result: String,
+    description: String,
+  },
+  { timestamps: true }
+);
+
+const questionnairSchema = new mongoose.Schema(
+  {
+    tags: [String],
+    questions: [questionSchema],
+    scores: [scoreSchema],
+    description: String,
+    rules: String,
+  },
+  { timestamps: true }
+);
+
+const questionnairModel = mongoose.model('Questionnair', questionnairSchema);
+
+module.exports = questionnairModel;
