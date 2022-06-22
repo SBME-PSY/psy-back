61ad3046b8c33e6fb19e66b2dd2ffd7844c373c3
saeedkhal <nnowyan@gmail.com>
2022-06-22 13:26:57 +0200

    add basic group therapy

--- server.js
+++ server.js
@@ -1,18 +1,54 @@
 const dotenv = require('dotenv');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+const http = require('http');
+const cors = require('cors');
+const socketIo = require('socket.io');
+const peer = require('peer');
 
+const app = require('./app');
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: true,
+    methods: ['GET', 'POST'],
+  },
+});
 dotenv.config({ path: './config/config.env' });
 const connectDB = require('./config/db');
 
 connectDB();
-const app = require('./app');
+const { PeerServer } = peer;
 
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () =>
-  console.log(`Listening on port ${PORT}`.yellow.bold)
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POSt'],
+  })
 );
+
+io.on('connection', (socket) => {
+  console.log('socket connected');
+  socket.on('join-room', (roomId, peerId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', peerId);
+    socket.on('disconnect', () => {
+      console.log('socket disconnected');
+      socket.to(roomId).emit('user-disconnect', peerId);
+    });
+  });
+});
+const peerPort = process.env.PEERPORT || 9000;
+const peerServer = PeerServer({ port: peerPort, path: '/myapp' });
+peerServer.on('connection', (client) => {
+  console.log('client connected ');
+});
+peerServer.on('disconnect', (client) => {
+  console.log('client disconnected ');
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
