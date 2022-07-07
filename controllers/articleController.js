d40e326b7090aa7b54eb6a4c681562425201f143
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2022-07-07 18:41:19 +0200

    feat: add searching for articles

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -4,27 +4,50 @@ const { articleValidators } = require('../validators');
 const { AppError, APIFeatures } = require('../utils');
 
 exports.createArticle = asyncHandler(async (req, res, next) => {
+  req.body.author = req.user.id;
+
   const { error, value } = articleValidators.articleSchemaValidator.validate(
     req.body
   );
+
   if (error) {
     return next(new AppError(error, 400));
   }
+
   const article = await articleModel.create(value);
+
   responseHandler.sendResponse(res, 200, 'sucess', article, null, null);
 });
+
 exports.getArticle = asyncHandler(async (req, res, next) => {
   const article = await articleModel.findById(req.params.id);
   responseHandler.sendResponse(res, 200, 'sucess', article, null, null);
 });
+
 exports.getAllArticles = asyncHandler(async (req, res, next) => {
-  const features = new APIFeatures(articleModel.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+  let features;
+  if (req.query.searchString) {
+    features = new APIFeatures(
+      articleModel
+        .find({
+          $text: { $search: req.query.searchString },
+          score: { $meta: 'textScore' },
+        })
+        .sort({ score: { $meta: 'textScore' } }),
+      req.query
+    )
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+  } else {
+    features = new APIFeatures(articleModel.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+  }
   const articles = await features.query;
 
-  // SEND RESPONSE
   responseHandler.sendResponse(res, 200, 'sucess', articles, null, null);
 });
