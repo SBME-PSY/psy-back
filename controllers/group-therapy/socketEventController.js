bb89231365bc60c95b28322d0db3cefdde2da9a2
saeedkhal <nnowyan@gmail.com>
2022-07-14 01:28:28 +0200

    add get roomEvents

--- controllers/group-therapy/socketEventController.js
+++ controllers/group-therapy/socketEventController.js
@@ -1,14 +1,19 @@
 const rooms = {};
 const socketEvents = (socket) => {
   console.log('socket connected');
-  socket.on('create-room', (roomId) => {
-    rooms[roomId] = socket.id;
+  socket.on('create-room', (roomId, peerId) => {
+    rooms[socket.id] = roomId;
     socket.join(roomId);
     console.log('room created');
     console.log(roomId);
+    socket.on('disconnect', () => {
+      console.log('socket disconnected');
+      socket.to(roomId).emit('user-disconnect', peerId);
+      delete rooms[socket.id];
+    });
   });
   socket.on('join-room', (roomId, peerId) => {
-    const roomExist = Object.keys(rooms).includes(roomId);
+    const roomExist = Object.values(rooms).includes(roomId);
     console.log('join room came ');
     if (roomExist) {
       socket.join(roomId);
@@ -24,6 +29,10 @@ const socketEvents = (socket) => {
   });
   socket.on('sentMessage', (message, roomId) => {
     socket.to(roomId).emit('messageCame', message);
+  });
+  socket.on('getRooms', (arg, callBack) => {
+    const roomsIds = Object.values(rooms);
+    callBack(roomsIds);
   });
 };
 module.exports = socketEvents;
